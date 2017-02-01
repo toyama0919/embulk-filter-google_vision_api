a9c6e48c896b8ff8892d1a515eea52f211f6b768
toyama0919 <toyama0919@gmail.com>
2017-02-01 14:41:20 +0900

    fix logging.

--- lib/embulk/filter/google_vision_api/vision_client.rb
+++ lib/embulk/filter/google_vision_api/vision_client.rb
@@ -30,7 +30,7 @@ module Embulk
 
         private
         def get_body(images)
-          Embulk.logger.info("processing #{@features.map{ |h| h['type'] }.join(',')} => #{images}")
+          Embulk.logger.info("Google Cloud Vision API #{@features.map{ |h| h['type'] }.join(',')} processing.. => #{images}")
           {
             requests: get_requests(images)
           }
